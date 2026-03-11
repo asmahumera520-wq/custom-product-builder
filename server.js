07f2ca59d8620c18596bce20361860a568b2d550
asmahumera520-wq <asmahumera520@gmail.com>
2026-03-11 12:02:39 +0530

    Initial backend upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+require("./config/db");
+
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", productRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
