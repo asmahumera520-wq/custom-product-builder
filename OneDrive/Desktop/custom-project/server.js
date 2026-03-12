c142e219d967cc268da77bfdb68dcf30d839d826
Prathyushap567 <prathyushap567@gmail.com>
2026-03-12 10:32:42 +0530

    Removed node_modules

---
+++ OneDrive/Desktop/custom-project/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
